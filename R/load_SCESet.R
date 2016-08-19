d5e47f70b1d3ea12b51d9932f72452e17ee8538c
Luke Zappia <lazappi@users.noreply.github.com>
2016-08-19 17:18:23 +1000

    Add alignment and counting stats
    Some of this is a bit weird, not sure why...

--- R/load_SCESet.R
+++ R/load_SCESet.R
@@ -11,6 +11,8 @@ loadKolodCounts <- function() {
     dir <- "/group/bioi1/shared/public_data/Kolodziejczyk-mESCs-scRNAseq/"
     data <- readr::read_tsv(file.path(dir, "counts/counts.txt"), comment = "#")
     sdrf <- readr::read_tsv(file.path(dir, "metadata/E-MTAB-2600.sdrf.txt"))
+    star <- readr::read_csv(file.path(dir, "stats/star_stats.txt"))
+    fcount <- readr::read_tsv(file.path(dir, "counts/counts.txt.summary"))
     is.dup <- duplicated(sdrf$"Comment[ENA_RUN]")
     sdrf <- sdrf[!is.dup, ]
 
@@ -49,14 +51,35 @@ loadKolodCounts <- function() {
     # Find bulk samples
     is.bulk <- sdrf$"Material Type" == "cells"
 
+    # Alignment stats
+    input <- star$InputNum
+    names(input) <- star$Sample
+    input <- input[samples]
+    mapped <- star$MappedNum
+    names(mapped) <- star$Sample
+    mapped <- mapped[samples]
+
+    # Counting stats
+    fcount <- as.data.frame(fcount)
+    colnames(fcount) <- c("Status", samples)
+    rownames(fcount) <- fcount$Status
+    fcount <- t(fcount[, -1])
+    assigned <- fcount[, "Assigned"]
+    assigned <- assigned[samples]
+
+    # Phenotype data
+    pheno <- data.frame(CellID = samples, Condition = conds, Plate = plates,
+                        Input = input, Mapped = mapped,
+                        pctMapped = (mapped / input) * 100, Assigned = assigned,
+                        pctAssigned = (assigned / input) * 100,
+                        pctMappedAssigned = (assigned / mapped) * 100)
+
     # Produce SCESet object
     kolod <- scater::newSCESet(countData = counts)
     Biobase::fData(kolod) <- as.data.frame(annot)
     Biobase::fData(kolod)$Gene <- genes
     rownames(Biobase::fData(kolod)) <- genes
-    Biobase::pData(kolod)$CellID <- samples
-    Biobase::pData(kolod)$Condition <- conds
-    Biobase::pData(kolod)$Plate <- plates
+    Biobase::pData(kolod) <- pheno
 
     # Remove bulk samples
     kolod <- kolod[, !is.bulk]
